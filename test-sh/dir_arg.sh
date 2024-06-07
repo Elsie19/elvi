ebb62954af49355196664afaea53a1d5b7a7a412
Elsie <elsie19@pm.me>
2024-06-07 15:18:29 -0500

    Ooops

--- test-sh/dir_arg.sh
+++ test-sh/dir_arg.sh
@@ -18,8 +18,6 @@ fi
 for FILE in "$DIRECTORY"/*; do
     if [ -f "$FILE" ]; then
         echo "$FILE is a regular file."
-    elif [ -d "$FILE" ]; then
-        echo "$FILE is a directory."
     else
         echo "$FILE is something else."
     fi
