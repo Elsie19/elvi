fd42c3fefa7a78c629626576aa6963894589a266
Elsie <elsie19@pm.me>
2024-06-20 13:09:39 -0500

    feat(env): add printing capabilities

--- src/internal/env.rs
+++ src/internal/env.rs
@@ -1,7 +1,16 @@
+use super::commands::HowRun;
+
 /// Struct to handle the global environment.
 pub struct Env {
     in_function: bool,
     pub subshells_in: u32,
+    pub stdout: String,
+    pub stderr: String,
+}
+
+pub enum Std {
+    Out,
+    Err,
 }
 
 impl Env {
@@ -15,6 +24,20 @@ impl Env {
     pub fn in_function(&self) -> bool {
         self.in_function
     }
+
+    /// Append or print text to screen
+    pub fn print(&mut self, whereto: Std, how: HowRun, text: &str) {
+        match how {
+            HowRun::RealTime => match whereto {
+                Std::Out => print!("{text}"),
+                Std::Err => eprint!("{text}"),
+            },
+            HowRun::Piped | HowRun::Substitution => match whereto {
+                Std::Out => self.stdout.push_str(text),
+                Std::Err => self.stderr.push_str(text),
+            },
+        }
+    }
 }
 
 impl Default for Env {
@@ -22,6 +45,8 @@ impl Default for Env {
         Self {
             in_function: false,
             subshells_in: 1,
+            stdout: String::new(),
+            stderr: String::new(),
         }
     }
 }
